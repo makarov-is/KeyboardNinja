aa8438beea67c611d9c0e4566890b4bb8825589c
Ilya Makarov <makarov7297@mail.ru>
2020-11-03 23:49:05 +0700

    Added graphics initialization function

--- code/kninja.cpp
+++ code/kninja.cpp
@@ -2,6 +2,31 @@
 #include <d2d1.h>
 
 ID2D1Factory *factory;
+ID2D1HwndRenderTarget *renderTarget;
+ID2D1SolidColorBrush *backgroundBrush;
+
+HRESULT initGraphicsResources(HWND window)
+{
+	HRESULT result = S_OK;
+
+	if(!renderTarget)
+	{
+		RECT clientRect;
+		GetClientRect(window, &clientRect);
+		D2D1_SIZE_U renderTargetSize = D2D1::SizeU(clientRect.right, clientRect.bottom);
+
+		result = factory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(),
+		                                         D2D1::HwndRenderTargetProperties(window, renderTargetSize), 
+		                                         &renderTarget);
+		if(SUCCEEDED(result))
+		{
+			D2D1_COLOR_F backgroundColor = D2D1::ColorF(0.0f, 0.0f, 0.0f);
+			result = renderTarget->CreateSolidColorBrush(backgroundColor, &backgroundBrush);
+		}
+	}
+
+	return(result);
+}
 
 LRESULT CALLBACK KNWindowProc(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -12,6 +37,11 @@ LRESULT CALLBACK KNWindowProc(HWND window, UINT msg, WPARAM wParam, LPARAM lPara
 		case WM_CREATE:
 		{
 			D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &factory);
+		} break;
+
+		case WM_PAINT:
+		{
+			initGraphicsResources(window);
 		} break;
 
 		case WM_CLOSE:
