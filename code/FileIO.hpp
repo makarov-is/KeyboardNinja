1b2ee6915f2a33faae42753e682b9de81d5da35a
Ilya Makarov <makarov7297@mail.ru>
2021-05-03 19:15:57 +0700

    Added FileIO module Added tests for FileIO module

---
+++ code/FileIO.hpp
@@ -0,0 +1,10 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include <windows.h>
+
+char *allocTemporaryBuffer(UINT bufferSize);
+bool checkFileSizeOK(UINT bytesRead, UINT outBufferSize);
+void readFile(const wchar_t *filename, WCHAR **outBuffer, UINT outBufferSize);
+
+#endif FILE_IO_H
